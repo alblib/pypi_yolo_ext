3d214fa7b5f26f9c171bd6f6b82c105ca8c15b2c
Woohyun Rim <rim.woohyun@illuni.com>
2025-09-21 19:27:23 +0900

    truncated yolo model prediction test

--- tests/test_truncate_classes.py
+++ tests/test_truncate_classes.py
@@ -1,10 +1,22 @@
-import unittest
-from src.yolo_ext import yolo_model_truncate_classes, imagenet_static_qdq_quantization
+import unittest, os
+from src.yolo_ext import yolo_model_truncate_classes, imagenet_static_qdq_quantization_onnx
 from ultralytics import YOLO
 
 
 class TestYOLOModelTruncateClasses(unittest.TestCase):
     def test_result(self):
         self.model = YOLO('yolo11n.pt')
-        self.model = yolo_model_truncate_classes(self.model, 'person')
-        self.model.export(format='onnx')
+        self.trunc_model = yolo_model_truncate_classes(self.model, 'person')
+
+        # Test if export successful
+        self.trunc_model.export(format='onnx')
+
+        # Test prediction and compare
+        orig_predictions = self.trunc_model.predict(
+            [os.path.join('data', name) for name in os.listdir('data')])
+        trun_predictions = self.trunc_model.predict(
+            [os.path.join('data', name) for name in os.listdir('data')])
+        for pred in orig_predictions:
+            pred.show()
+        for pred in trun_predictions:
+            pred.show()
